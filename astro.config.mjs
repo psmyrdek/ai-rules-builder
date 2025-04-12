54bd07d0230a57bdf68e7455ec9002b1bae3d86a
psmyrdek <przemek.smyrdek@gmail.com>
2025-04-12 21:41:44 +0200

    chore: use cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,7 +25,5 @@ export default defineConfig({
     enabled: false,
   },
   integrations: [react()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 });
